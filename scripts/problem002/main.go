a34c57ee839eb1f49962ee1ca81be8f22f14b192
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-22 14:57:56 -0700

    [go] problem002 start (fib only)

---
+++ scripts/problem002/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Sum of even fibonacci numbers below 4,000,000")
+
+	// list of fibonacci numbers
+	fib := []int{1, 1}
+	// sum := 0
+
+	//calculate the fibonacci numbers below 4m
+	for i := 2; ; i++ {
+		// calculate the next number from the sum of the prior two
+		next := fib[i-1] + fib[i-2]
+
+		// break if the next number is larger than the limit
+		if next >= 4_000_000 {
+			break
+		}
+
+		// append the number
+		fib = append(fib, next)
+	}
+
+	// concatenate the list and print it
+	fmt.Println(fib)
+
+}
