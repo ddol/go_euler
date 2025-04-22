bc2ab24c9178ed8e71a8ec4f2439b57f8bf78dc4
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-21 17:55:41 -0700

    [go] solve 001

--- scripts/problem001/main.go
+++ scripts/problem001/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("The sum of all multiples of 3 or 5 below 1000")
+	sum := 0
+	var max_number int
+
+	fmt.Println("The sum of all multiples of 3 or 5, below a limit. Enter limit:")
+	// read the limit from the user
+	fmt.Scanf("%d", &max_number)
+
+	for i := 0; i < max_number; i++ {
+		// check if the number modulus 3 or 5 is zero, if so there is no remainder
+		// and the number is a multiple of 3 or 5
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+
+	fmt.Println("The sum is: ", sum)
 }
