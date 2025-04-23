d152c76376486b7c8320acec02eb8609ea04cd5e
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-23 12:15:07 -0700

    [go] remove user input

--- scripts/problem001/main.go
+++ scripts/problem001/main.go
@@ -4,12 +4,11 @@ import "fmt"
 
 func main() {
 	sum := 0
-	var max_number int
+	max_number := 1000
 
-	fmt.Println("The sum of all multiples of 3 or 5, below a limit. Enter limit:")
-	// read the limit from the user
-	fmt.Scanf("%d", &max_number)
+	fmt.Println("The sum of all multiples of 3 or 5, below limit:", max_number)
 
+	// iterate from 0 to the max number
 	for i := 0; i < max_number; i++ {
 		// check if the number modulus 3 or 5 is zero, if so there is no remainder
 		// and the number is a multiple of 3 or 5
