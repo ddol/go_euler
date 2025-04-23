ca4520b8c0799b335b540ca0b294b0a080dbb7a1
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-23 12:12:28 -0700

    [go] solve 003, add LargestPrimeFactor

--- pkg/utils/math.go
+++ pkg/utils/math.go
@@ -1,7 +1,7 @@
 package utils
 
 // Generate a slice of primes up to a given limit using the Sieve of Eratosthenes
-func Primes(limit int) []int {
+func Primes(limit int64) []int {
 	// if the limit is less than 2, return an empty slice
 	if limit < 2 {
 		return []int{}
@@ -18,27 +18,61 @@ func Primes(limit int) []int {
 	sieve[1] = false
 
 	// mark all other numbers as prime
-	for i := 2; i <= limit; i++ {
+	for i := 2; i <= int(limit); i++ {
 		sieve[i] = true
 	}
 
 	// iterate over the sieve, starting at 2 and stopping at the square root of the limit
-	for i := 2; i*i <= limit; i++ {
+	for i := 2; i*i <= int(limit); i++ {
 		// only continue if the value is still marked as prime
 		if sieve[i] {
 			// iterate over the multiples of the value, marking them as not prime
-			for j := i * i; j <= limit; j += i {
+			for j := i * i; j <= int(limit); j += i {
 				sieve[j] = false
 			}
 		}
 	}
 
 	// iterate over the sieve, if the value is true add the prime for that index to the primes slice
-	for i := 2; i <= limit; i++ {
+	for i := 2; i <= int(limit); i++ {
 		if sieve[i] {
 			primes = append(primes, i)
 		}
 	}
 
 	return primes
+}
+
+func LargestPrimeFactor(n int64) int64 {
+	// Special case for factor 2 (only even prime)
+	for n%2 == 0 {
+		n /= 2
+	}
+
+	// If n becomes 1, return 2 as the largest prime factor
+	if n == 1 {
+		return 2
+	}
+
+	// Check odd numbers only, starting from 3
+	factor := int64(3)
+
+	// Iterate through odd factors starting from 3
+	// Stop when factor*factor exceeds n
+	for factor*factor <= n {
+		// If n is evenly divisible by factor (mod zero), divide n by factor and continue to check for larger factors
+		if n%factor == 0 {
+			n /= factor
+		} else {
+			// Skip even numbers
+			factor += 2
+		}
+	}
+
+	// If n > 1, n is the largest prime factor
+	if n > 1 {
+		return n
+	}
+
+	return factor
 }
--- scripts/problem003/main.go
+++ scripts/problem003/main.go
@@ -6,18 +6,11 @@ import (
 )
 
 func main() {
-	// big_prime := 600851475143
-	big_prime := 13195
+	// big_prime := int64(13195)
+	big_prime := int64(600851475143)
 
 	fmt.Println("Calculating the largest prime factor of", big_prime)
 
-	// create a slice to hold the prime factors
-	// factors := []int{}
-
-	// use the Primes function from the utils math package to generate a slice of primes up to the limit
-	primes := utils.Primes(big_prime)
-
-	// print the primes
-	fmt.Println("Primes: ", primes)
-
+	// print the last prime
+	fmt.Println("Largest prime factor:", utils.LargestPrimeFactor(big_prime))
 }
