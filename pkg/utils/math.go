f360b6bd9d63a4a9c05c6ddae495f3384e4eba0f
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-23 11:51:48 -0700

    [go] start primes function

--- pkg/utils/math.go
+++ pkg/utils/math.go
@@ -1,1 +1,33 @@
 package utils
+
+// Generate a slice of primes up to a given limit using the Sieve of Eratosthenes
+func Primes(limit int) []int {
+	// if the limit is less than 2, return an empty slice
+	if limit < 2 {
+		return []int{}
+	}
+
+	// create a slice to hold the primes
+	primes := []int{}
+
+	// generate a slice of booleans, initialized to true, up to the limit
+	sieve := make([]bool, limit+1)
+
+	// mark 0 and 1 as not prime
+	sieve[0] = false
+	sieve[1] = false
+
+	// mark all other numbers as prime
+	for i := 2; i <= limit; i++ {
+		sieve[i] = true
+	}
+
+	// iterate over the sieve, if the value is true add the prime for that index to the primes slice
+	for i := 2; i <= limit; i++ {
+		if sieve[i] {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
--- scripts/problem003/main.go
+++ scripts/problem003/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"euler-go/pkg/utils"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("This is going to be a problem...")
+	// big_prime := 600851475143
+	big_prime := 13195
+
+	fmt.Println("Calculating the largest prime factor of", big_prime)
+
+	// create a slice to hold the prime factors
+	// factors := []int{}
+
+	// use the Primes function from the utils math package to generate a slice of primes up to the limit
+	primes := utils.Primes(big_prime)
+
+	// print the primes
+	fmt.Println("Primes: ", primes)
+
 }
