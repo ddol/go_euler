299fbe04ebed5ce4b5d8c8b70c8b3280adee018e
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-29 15:32:16 -0700

    [go] solve 005

--- scripts/problem005/main.go
+++ scripts/problem005/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	// "euler-go/pkg/utils"
+	"euler-go/pkg/utils"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("This is going to be a problem...")
+	smallest := int64(1)
+
+	limit := 20
+
+	fmt.Println("Finding the smallest number evenly divisible by all integers up to", limit)
+
+	// moving in increments of 210 (2*3*5*7) efficiently find the smallest number that is evenly divisible by all numbers from 1 to limit
+	for i := int64(210); ; i += 210 {
+		if utils.DivisibleUpTo(i, limit) {
+			smallest = i
+			break
+		}
+	}
+
+	fmt.Println("The smallest number evenly divisible by all integers up to", limit, "is", smallest)
+
 }
