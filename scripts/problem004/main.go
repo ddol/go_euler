41a5a16550b625e6625316b3dd73bca98394fd42
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-28 18:23:18 -0700

    [go] solve 004

--- scripts/problem004/main.go
+++ scripts/problem004/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"euler-go/pkg/utils"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("This is going to be a problem...")
+	largest_palindrome := int64(0)
+
+	// We assume the factors are 9xx to save compute time
+	for i := 900; i < 1000; i++ {
+		for j := 900; j < 1000; j++ {
+			product := int64(i * j)
+
+			// check if the product is a palindrome
+			if utils.IsPalindromeNumber(product) {
+				// check if the product is larger than the current largest palindrome
+				if product > largest_palindrome {
+					largest_palindrome = product
+				}
+			}
+		}
+	}
+
+	fmt.Println("Largest palindromic product of two 3-digit numbers is: ", largest_palindrome)
 }
