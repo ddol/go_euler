1ce4c4d451d6a69b1cf18ea0901d170da36fcef8
David Dolphin <445312+ddol@users.noreply.github.com>
2025-04-30 17:33:15 -0700

    [go] solve 007

--- scripts/problem007/main.go
+++ scripts/problem007/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	// "euler-go/pkg/utils"
+	"euler-go/pkg/utils"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("This is going to be a problem...")
+	prime_number := 10_001
+
+	fmt.Println("Calculating primes up to prime number", prime_number)
+
+	prime := utils.GenerateNthPrime(prime_number)
+
+	fmt.Println("The", prime_number, "th prime number is", prime)
 }
