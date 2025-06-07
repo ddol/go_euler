526cc323493b05d3c030bceacabd354239c43585
David Dolphin <445312+ddol@users.noreply.github.com>
2025-06-06 17:07:00 -0700

    [go] init triangle (018)

---
+++ scripts/problem018/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"euler-go/pkg/utils"
+	"fmt"
+)
+
+func makeTriangle() {
+	depth := 4
+
+	triangle := make([][]int, depth)
+	for i := range triangle {
+		triangle[i] = make([]int, i+1)
+	}
+
+	// initialise the triangle using slices for each row
+	triangle[0] = []int{3}
+	triangle[1] = []int{7, 4}
+	triangle[2] = []int{2, 4, 6}
+	triangle[3] = []int{8, 5, 9, 3}
+}
+
+func main() {
+	fmt.Println("This is going to be a problem, Number", utils.SumRange(4))
+}
