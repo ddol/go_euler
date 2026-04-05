e442d6d292915a430958d4523ea19275fef768b2
David Dolphin <445312+ddol@users.noreply.github.com>
2026-04-05 14:27:06 -0400

    [go] solve 019

---
+++ scripts/problem019/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Date(1901, time.January, 1, 0, 0, 0, 0, time.UTC)
+	end := time.Date(2000, time.December, 31, 0, 0, 0, 0, time.UTC)
+
+	sundays := 0
+
+	fmt.Printf("date range: %s to %s\n", start, end)
+
+	for d := start; end.Compare(d) == 1; d = d.Add(86400000000000) {
+		if d.Weekday() == time.Sunday {
+			sundays += 1
+		}
+	}
+
+	fmt.Println("sunday count:", sundays)
+}
